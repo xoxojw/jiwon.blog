4f20c9326d6f7c3b58fb9bee332bb5f261ad41bc
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-02 10:52:24 +0900

    :recycle: Navigation, NavLink 컴포넌트 리팩토링

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,16 +9,14 @@ import ProgressBar from "../app/blog/components/ProgressBar";
 const Navigation = () => {
   const pathname = usePathname();
   return (
-    <header
-      className="sticky top-0 z-20 w-screen h-20 bg-inherit"
-    >
+    <header className="sticky top-0 z-20 w-screen h-20 bg-inherit">
       {pathname.includes("blog/") ? <ProgressBar /> : null}
       <nav className="max-w-4xl mx-auto flex items-center gap-3 p-8">
         <ul className="w-full flex items-center justify-between">
           <div className="flex items-center gap-5">
             {NAV_LISTS.map(({ name, href }) => (
               <li key={name}>
-                <NavLink href={href}>
+                <NavLink pathname={pathname} href={href}>
                   {name}
                 </NavLink>
               </li>
--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
 
 import { cls } from "@/src/libs/util";
 
 type NavLinkProps = {
+  pathname: string;
   href: string;
   children: ReactNode;
 };
 
-const NavLink = ({ href, children }: NavLinkProps) => {
-  const pathname = `/${usePathname().split("/")[1]}`; // active paths on dynamic subpages
-  const active = pathname === href;
+const NavLink = ({ pathname, href, children }: NavLinkProps) => {
+  const dynamicPathname = `/${pathname.split("/")[1]}`; // active paths on dynamic subpages
+  const active = dynamicPathname === href;
 
   return (
     <Link
