97f7816beddbab7bea83f09750fd537ef1b5a160
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-02 19:59:27 +0900

    :dizzy: 프로그레스바 애니메이션 추가

--- src/app/blog/components/ProgressBar.tsx
+++ src/app/blog/components/ProgressBar.tsx
@@ -1,12 +1,17 @@
 "use client";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const ProgressBar = () => {
   const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
   return (
     <motion.div
       className="fixed top-0 left-0 right-0 h-2 bg-primary-300 dark:bg-primary origin-[0] z-20"
-      style={{ scaleX: scrollYProgress }}
+      style={{ scaleX }}
     />
   );
 };
