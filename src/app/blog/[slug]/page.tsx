29497527d7cdd226acf1b4ca6a45dd2d6b3235a6
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-02 10:37:08 +0900

    :lipstick: 게시글 페이지 애니메이션 수정

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -20,16 +20,19 @@ const BlogPage = ({ params }: { params: { slug: string }; }) => {
       <ProgressBar />
       <section className="flex flex-col gap-8">
         <div className="animate-in flex flex-col gap-4 border-b pb-8">
-          <div className="animate-in flex flex-col md:flex-row md:justify-between md:items-center gap-2 md:gap-0">
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-2 md:gap-0">
             <h1 className="font-bold text-3xl">
               {post.title}
             </h1>
             <Tags tags={post.tags} />
           </div>
-          <PublishedDate publishedAt={post.publishedAt} dateFormat="LLLL d, yyyy" />
-          <blockquote>{post.summary}</blockquote>
+          <PublishedDate animateIndex={1} publishedAt={post.publishedAt} dateFormat="LLLL d, yyyy" />
+          <blockquote className="animate-in" style={{ "--index": 2 } as React.CSSProperties}>{post.summary}</blockquote>
         </div>
-        <div className="animate-in prose dark:prose-invert max-w-none" style={{ "--index": 1 } as React.CSSProperties}>
+        <div
+          className="animate-in prose dark:prose-invert max-w-none"
+          style={{ "--index": 3 } as React.CSSProperties}
+        >
           <MdxWrapper code={post.body.code} />
         </div>
       </section>
--- src/components/ui/PublishedDate.tsx
+++ src/components/ui/PublishedDate.tsx
@@ -1,13 +1,18 @@
+import { cls } from "@/src/libs/util";
 import { format, parseISO } from "date-fns";
 
 type Props = {
+  animateIndex?: number;
   publishedAt: string;
   dateFormat?: string;
 }
 
-const PublishedDate = ({ publishedAt, dateFormat = "yyyy-MM-dd" }: Props) => {
+const PublishedDate = ({ animateIndex, publishedAt, dateFormat = "yyyy-MM-dd" }: Props) => {
   return (
-    <p className="text-sm text-gray-500">
+    <p
+      className={cls("text-sm text-gray-500", `${animateIndex ? "animate-in" : ""}`)}
+      style={{ "--index": animateIndex } as React.CSSProperties}
+    >
       {format(parseISO(publishedAt), dateFormat)}
     </p>
   );
